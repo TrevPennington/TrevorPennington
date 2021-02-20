0ca969661b232645b7fe97c6376fa24845aa67e2
Trevor Pennington <trevorpennington@trevors-mbp.lan>
2021-02-20 13:11:20 -0500

    gatsby bug

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import styled from 'styled-components';
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
